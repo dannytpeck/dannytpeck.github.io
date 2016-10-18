178a36bb19d677686452192f9bee9b0a9853a274
Danny Peck <dannytpeck@gmail.com>
2016-10-18 14:33:25 -0700

    Fix bug in hamburger menu

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 // smooth page scroll to to anchor (from https://css-tricks.com/snippets/jquery/smooth-scrolling/)
-$(function() {
+ $(function() {
   $('a[href*="#"]:not([href="#"])').click(function() {
     if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
       var target = $(this.hash);
